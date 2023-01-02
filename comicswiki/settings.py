1864c7b3afdbca165d06defe5aca21fe8814e483
Connor Goodall <burningshock83@gmail.com>
2023-01-01 20:42:28 -0500

    Added the password reset feature with gmail

--- comicswiki/settings.py
+++ comicswiki/settings.py
@@ -135,3 +135,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/comics/'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config("EMAIL_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
