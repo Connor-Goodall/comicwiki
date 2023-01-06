e477395126697e949f8c3daee9eb90a3bc401c31
Connor Goodall <burningshock83@gmail.com>
2023-01-06 15:31:57 -0500

    Added a footer that gives credit to the people that helped me create the website

--- comics/admin.py
+++ comics/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import superheroBiography, superheroAppearance, superheroWork, superheroConnections, superheroPowerstats
+from .models import superheroImages
 
 # Register your models here.
+admin.site.register(superheroBiography)
+admin.site.register(superheroAppearance)
+admin.site.register(superheroWork)
+admin.site.register(superheroConnections)
+admin.site.register(superheroPowerstats)
+admin.site.register(superheroImages)
