db50a3ab8c9f5d181f6b29db540499b0fe9f4ca6
Connor Goodall <burningshock83@gmail.com>
2023-01-09 16:46:11 -0500

    Added the tests for all parts of the superhero appearance model

--- comics/tests.py
+++ comics/tests.py
@@ -113,3 +113,96 @@ class EditSuperheroBiographyTest(TestCase):
 
     def tearDown(self):
         self.driver.quit()
+
+class EditSuperheroAppearanceTest(TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get('http://127.0.0.1:8000/login/')
+        username = self.driver.find_element(By.XPATH, "//input[@name='username']")
+        username.send_keys("test")
+        password = self.driver.find_element(By.XPATH, "//input[@name='password']")
+        password.send_keys("TestPassword123!")
+        login = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        login.click()
+        self.driver.get('http://127.0.0.1:8000/comics/Spider-Man (Miles Morales)/edit/')
+    def test_changeGender(self):
+        gender = self.driver.find_element(By.XPATH, "//input[@name='gender']")
+        length = len(gender.get_attribute("value"))
+        count = 0
+        while (count < length):
+            gender.send_keys(Keys.BACKSPACE)
+            count += 1
+        gender.send_keys("Male")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert 'Gender: Male' in self.driver.page_source
+
+    def test_changeRace(self):
+        race = self.driver.find_element(By.XPATH, "//input[@name='race']")
+        length = len(race.get_attribute("value"))
+        count = 0
+        while (count < length):
+            race.send_keys(Keys.BACKSPACE)
+            count += 1
+        race.send_keys("Human")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert 'Race: Human' in self.driver.page_source
+    def test_changeHeight(self):
+        height = self.driver.find_element(By.XPATH, "//input[@name='height']")
+        length = len(height.get_attribute("value"))
+        count = 0
+        while (count < length):
+            height.send_keys(Keys.BACKSPACE)
+            count += 1
+        height.send_keys("5'8 or 172.7 cm")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert "Height: 5'8 or 172.7 cm" in self.driver.page_source
+    def test_changeWeight(self):
+        weight = self.driver.find_element(By.XPATH, "//input[@name='weight']")
+        length = len(weight.get_attribute("value"))
+        count = 0
+        while (count < length):
+            weight.send_keys(Keys.BACKSPACE)
+            count += 1
+        weight.send_keys("160.06 lbs or 72.6 kg")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert "Weight: 160.06 lbs or 72.6 kg" in self.driver.page_source
+    def test_changeEyeColor(self):
+        eyeColor = self.driver.find_element(By.XPATH, "//input[@name='eyeColor']")
+        length = len(eyeColor.get_attribute("value"))
+        count = 0
+        while (count < length):
+            eyeColor.send_keys(Keys.BACKSPACE)
+            count += 1
+        eyeColor.send_keys("Brown")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert "Eye Color: Brown" in self.driver.page_source
+    def test_changeHairColor(self):
+        hairColor = self.driver.find_element(By.XPATH, "//input[@name='hairColor']")
+        length = len(hairColor.get_attribute("value"))
+        count = 0
+        while (count < length):
+            hairColor.send_keys(Keys.BACKSPACE)
+            count += 1
+        hairColor.send_keys("Black")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert "Hair Color: Black" in self.driver.page_source
+    def tearDown(self):
+        self.driver.quit()
