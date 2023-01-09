9ecb7e02d8077ce8fe890ddace88f52c4de4cca5
Connor Goodall <burningshock83@gmail.com>
2023-01-09 15:59:41 -0500

    Added the tests for editing all the fields for the superhero biography model

--- comics/tests.py
+++ comics/tests.py
@@ -1,3 +1,115 @@
+import time
+
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # Create your tests here.
+
+class EditNoLoginTest(TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get('http://127.0.0.1:8000/comics/Spider-Man (Miles Morales)/edit/')
+    def test_correctPage(self):
+        assert 'Log in' in self.driver.page_source
+
+    def tearDown(self):
+        self.driver.quit()
+
+class EditSuperheroBiographyTest(TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get('http://127.0.0.1:8000/login/')
+        username = self.driver.find_element(By.XPATH, "//input[@name='username']")
+        username.send_keys("test")
+        password = self.driver.find_element(By.XPATH, "//input[@name='password']")
+        password.send_keys("TestPassword123!")
+        login = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        login.click()
+        self.driver.get('http://127.0.0.1:8000/comics/Spider-Man (Miles Morales)/edit/')
+    def test_correctPage(self):
+        assert 'Biography' in self.driver.page_source
+
+    def test_changeFullName(self):
+        fullName = self.driver.find_element(By.XPATH, "//input[@name='fullName']")
+        length = len(fullName.get_attribute("value"))
+        count = 0
+        while (count < length):
+            fullName.send_keys(Keys.BACKSPACE)
+            count += 1
+        fullName.send_keys("Miles Gonzalo Morales")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert 'Full Name: Miles Gonzalo Morales' in self.driver.page_source
+    def test_changeAlterEgos(self):
+        alterEgos = self.driver.find_element(By.XPATH, "//input[@name='alterEgos']")
+        length = len(alterEgos.get_attribute("value"))
+        count = 0
+        while (count < length):
+            alterEgos.send_keys(Keys.BACKSPACE)
+            count += 1
+        alterEgos.send_keys("Captain Universe")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert 'Alter Egos: Captain Universe' in self.driver.page_source
+
+    def test_changeAliases(self):
+        aliases = self.driver.find_element(By.XPATH, "//input[@name='aliases']")
+        length = len(aliases.get_attribute("value"))
+        count = 0
+        while (count < length):
+            aliases.send_keys(Keys.BACKSPACE)
+            count += 1
+        aliases.send_keys("Spiderman")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert 'Aliases: Spiderman' in self.driver.page_source
+    def test_changeBirthPlace(self):
+        birthPlace = self.driver.find_element(By.XPATH, "//input[@name='birthPlace']")
+        length = len(birthPlace.get_attribute("value"))
+        count = 0
+        while (count < length):
+            birthPlace.send_keys(Keys.BACKSPACE)
+            count += 1
+        birthPlace.send_keys("Forest Hills, New York")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert 'Birth Place: Forest Hills, New York' in self.driver.page_source
+    def test_changeFirstAppearance(self):
+        firstAppearance = self.driver.find_element(By.XPATH, "//input[@name='firstAppearance']")
+        length = len(firstAppearance.get_attribute("value"))
+        count = 0
+        while (count < length):
+            firstAppearance.send_keys(Keys.BACKSPACE)
+            count += 1
+        firstAppearance.send_keys("Ultimate Comics Fallout #4 (October, 2011)")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert 'First Appearance: Ultimate Comics Fallout #4 (October, 2011)' in self.driver.page_source
+    def test_changePublisher(self):
+        publisher = self.driver.find_element(By.XPATH, "//input[@name='publisher']")
+        length = len(publisher.get_attribute("value"))
+        count = 0
+        while (count < length):
+            publisher.send_keys(Keys.BACKSPACE)
+            count += 1
+        publisher.send_keys("Marvel Comics")
+        update = self.driver.find_element(By.XPATH, "//button[contains(@class, 'btn btn-outline-info')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", update)
+        time.sleep(2)
+        update.click()
+        assert 'Publisher: Marvel Comics' in self.driver.page_source
+
+    def tearDown(self):
+        self.driver.quit()
